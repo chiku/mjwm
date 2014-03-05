1b8c97fa1d647dff99bb8d91c0e6d678183b9a1f
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-03-05 19:24:18 +0530

    Rename itm to more specific entry

--- src/mjm.c
+++ src/mjm.c
@@ -11,13 +11,13 @@ Copyright (C) 2013 Chirantan Mitra <chirantan.mitra@gmail.com>
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "./mjm.h"
+#include "mjm.h"
 
-struct itm {
-	char exec[256];
-	char icon[256];
+struct entry {
 	char name[256];
-	char cate[256];
+	char executable[256];
+	char icon[256];
+	char category[256];
 };
 
 
@@ -26,20 +26,20 @@ int main(int argc, char *argv[]) {
 	char *scandir = "/usr/share/applications/";
 	char *outfile = "./automenu";
 	int iitm=0, i, aexp=0;
-	itm *itms;
+	entry *itms;
 
 
 	for (i=1; i<argc; i++) {
 		if (strcmp(argv[i], "-o")==0 && i+1<argc) {outfile = argv[i+1];}
 		if (strcmp(argv[i], "-s")==0 && i+1<argc) {scandir = argv[i+1];}
 		if (strcmp(argv[i], "-a")==0 ) {aexp = 1;}
-		if (strcmp(argv[i], "-h")==0 && i==1) {Dspout();}
+		if (strcmp(argv[i], "-h")==0 && i==1) {display_help(); exit(0);}
 	}
 
 
 	iitm = Reader(scandir, NULL);
 	if (iitm) {
-		itms = (itm*)malloc( sizeof( itm ) * iitm );	
+		itms = (entry*)malloc( sizeof( entry ) * iitm );
 		if (itms) {
 			iitm = Reader(scandir, itms);
 			Itmsrt(iitm, itms);
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
 }
 
 
-int Reader(char *scandir, itm *itms) {
+int Reader(char *scandir, entry *itms) {
 
 	DIR *dir;
 	FILE *fp;
@@ -65,7 +65,7 @@ int Reader(char *scandir, itm *itms) {
 	char *sexec = "Exec", *sicon = "Icon", *sname = "Name", *scate = "Categories";
 	char sline[1024], stmp[1024];
 	char *sbuff;
-	itm *itmp;
+	entry *itmp;
 	int ictr = 0, ectr = 0;
 	
 	dir=opendir(scandir);
@@ -73,7 +73,7 @@ int Reader(char *scandir, itm *itms) {
 	if (!dir) {
 			ectr++;
 	} else {
-		itmp = (itm*)malloc(sizeof(itm));
+		itmp = (entry*)malloc(sizeof(entry));
 		if (!itmp) {
 				ectr++;
 		} else {
@@ -88,29 +88,29 @@ int Reader(char *scandir, itm *itms) {
 				break;
 			}
 			
-			strcpy(itmp->exec, "");
+			strcpy(itmp->executable, "");
 			strcpy(itmp->icon, "");
 			strcpy(itmp->name, "");
-			strcpy(itmp->cate, "");
+			strcpy(itmp->category, "");
 			
 			while (fgets(sline, 256, fp) != NULL) {
 				sbuff = NULL;
 				sbuff = strtok(sline, "=");
 				if (sbuff) {
-				if (strcmp(sbuff, sexec)==0) { strcpy(itmp->exec, strtok(NULL, "\n")); }
+				if (strcmp(sbuff, sexec)==0) { strcpy(itmp->executable, strtok(NULL, "\n")); }
 				if (strcmp(sbuff, sicon)==0) { strcpy(itmp->icon, strtok(NULL, "\n")); }
 				if (strcmp(sbuff, sname)==0) { strcpy(itmp->name, strtok(NULL, "\n")); }
-				if (strcmp(sbuff, scate)==0) { strcpy(itmp->cate, strtok(NULL, "\n")); }
+				if (strcmp(sbuff, scate)==0) { strcpy(itmp->category, strtok(NULL, "\n")); }
 				}
 			}
 
-			if (strlen(itmp->exec) && strlen(itmp->icon) && strlen(itmp->name)) {
+			if (strlen(itmp->executable) && strlen(itmp->icon) && strlen(itmp->name)) {
 				ictr++;
 				if (itms) {
-					strcpy((itms+ictr-1)->exec, itmp->exec);
+					strcpy((itms+ictr-1)->executable, itmp->executable);
 					strcpy((itms+ictr-1)->icon, itmp->icon);
 					strcpy((itms+ictr-1)->name, itmp->name);
-					strcpy((itms+ictr-1)->cate, itmp->cate);
+					strcpy((itms+ictr-1)->category, itmp->category);
 				} 
 			}
 			
@@ -127,7 +127,7 @@ int Reader(char *scandir, itm *itms) {
 }
 
 
-int Rcwrite(int iitm, itm *itms, char *outfile, int aexp) {
+int Rcwrite(int iitm, entry *itms, char *outfile, int aexp) {
 
 FILE *wfp;
 int i, rslt;
@@ -143,7 +143,7 @@ if (aexp) {
 		rslt = 1;
 	} else { 
 		for (i=0 ; i<iitm ; i++) {
-		fprintf(wfp, sprt, (itms+i)->name, (itms+i)->icon, (itms+i)->exec);
+		fprintf(wfp, sprt, (itms+i)->name, (itms+i)->icon, (itms+i)->executable);
 		}
 		fclose(wfp);
 		rslt = 0;
@@ -152,10 +152,10 @@ if (aexp) {
 }
 
 
-int Itmsrt(int iitm, itm *itms) {
+int Itmsrt(int iitm, entry *itms) {
 
 	int i, j;
-	itm stmp;
+	entry stmp;
 
 	for (i=0; i<iitm-1; i++) {
 			for (j=i+1; j<iitm; j++) {
@@ -170,7 +170,6 @@ int Itmsrt(int iitm, itm *itms) {
 }
 
 
-void Dspout() {
 void display_help() {
 	printf("mjm2\n");
 	printf("Version 1.0\n");
@@ -181,6 +180,4 @@ void display_help() {
 	printf("  -h   Show this help\n\n");
 	printf("use it including the made file in the rootmenu section of your system.jwmrc\n");
 	printf("more information -> http://mjm.sourceforge.net/ and http://insm.cocolog-nifty.com/\n");
- 
-	exit(0);
 }
--- src/mjm.h
+++ src/mjm.h
@@ -5,9 +5,9 @@ Copyright (C) 2010 insmyic <gminsm@gmail.com>
 Copyright (C) 2013 Chirantan Mitra <chirantan.mitra@gmail.com>
 */
 
-typedef struct itm itm;
+typedef struct entry entry;
 
-int Reader(char *scandir, itm *itms);
-int Rcwrite(int iitm, itm *itms, char *outfile, int aexp);
-int Itmsrt(int iitm, itm *itms);
-void Dspout(void);
+int Reader(char *scandir, entry *itms);
+int Rcwrite(int iitm, entry *itms, char *outfile, int aexp);
+int Itmsrt(int iitm, entry *itms);
+void display_help(void);
