78945afd98250a377f01347249dd34c9e8ea22a8
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-03-10 15:28:07 +0530

    Update LICENSE headers in all source & test files

--- src/menu_entry.cpp
+++ src/menu_entry.cpp
@@ -1,8 +1,23 @@
 /*
-mjwm creates jwm's menu from (freedesktop) desktop files
-Include the generated file in the rootmenu section of your ~/.jwmrc
-Based on work by insmyic <gminsm@gmail.com> http://sourceforge.net/projects/mjm
-Copyright (C) 2014 Chirantan Mitra <chirantan.mitra@gmail.com>
+    This file is part of mjwm.
+    Copyright (C) 2014  Chirantan Mitra <chirantan.mitra@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+    Based on work by insmyic <gminsm@gmail.com> http://sourceforge.net/projects/mjm
 */
 
 #include <iostream>
