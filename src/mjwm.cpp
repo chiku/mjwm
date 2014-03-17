54f959b949e6f92a9828ba29e4a6ad9d76c16975
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-03-17 22:40:36 +0530

    Adds option for displaying application version
    -v or --version command-line options display the application version.
    Application follows semantic versioning. The new version is 2.0.0 (instead of 2.0)
    
    Fixes #19

--- src/mjwm.cpp
+++ src/mjwm.cpp
@@ -28,37 +28,49 @@
 #include "menu_entry.h"
 #include "menu_group.h"
 
-void display_help() {
-	std::cout << "mjwm version 2.1, Copyright (C) 2014 Chirantan Mitra <chirantan.mitra@gmail.com>" << std::endl;
+static const std::string VERSION = "2.1.0";
+
+static void display_help()
+{
+	std::cout << "mjwm version " << VERSION <<  ", Copyright (C) 2014 Chirantan Mitra <chirantan.mitra@gmail.com>" << std::endl;
 	std::cout << "mjwm comes with ABSOLUTELY NO WARRANTY; for details refer COPYING." << std::endl;
 	std::cout << "This is free software, and you are welcome to redistribute it" << std::endl;
 	std::cout << "under certain conditions; Refer COPYING for details." << std::endl;
 
 	std::cout << "mjwm creates jwm's menu from (freedesktop) desktop files" << std::endl;
-	std::cout << "  -o, --output-file       Outfile file [Default: ./automenu]" << std::endl;
-	std::cout << "  -s, --input-directory   Directory to scan for '.desktop' files [Default: /usr/share/applications/]" << std::endl;
-	std::cout << "  -a, --append-png        Add '.png' to icon filenames" << std::endl;
-	std::cout << "  -h, --help              Show this help" << std::endl << std::endl;
+	std::cout << "  -o, --output-file [FILE]           Outfile file [Default: ./automenu]" << std::endl;
+	std::cout << "  -s, --input-directory [DIRECTORY]  Directory to scan for '.desktop' files [Default: /usr/share/applications/]" << std::endl;
+	std::cout << "  -a, --append-png                   Add '.png' to icon filenames" << std::endl;
+	std::cout << "  -v, --version                      Show version" << std::endl << std::endl;
+	std::cout << "  -h, --help                         Show this help" << std::endl << std::endl;
 
 	std::cout << "Include the generated file in the rootmenu section of your ~/.jwmrc" << std::endl;
 	std::cout << "More information at http://github.com/chiku/mjwm" << std::endl;
 }
 
-void display_option_error() {
+static void display_version()
+{
+	std::cout << "mjwm " << VERSION << std::endl;
+}
+
+static void display_option_error()
+{
 	std::cerr << "Please run mjwm -h to see options" << std::endl;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	std::string directory_to_scan("/usr/share/applications/");
 	std::string output_filename("./automenu");
 	std::string icon_extension("");
 
-	const char* short_options = "o:s:ah";
+	const char* short_options = "o:s:avh";
 	const option long_options[] =
 	{
 		{"output-file",     required_argument, 0, 'o'},
 		{"input-directory", required_argument, 0, 's'},
 		{"append-png",      no_argument,       0, 'a'},
+		{"version",         no_argument,       0, 'v'},
 		{"help",            no_argument,       0, 'h'},
 		{0, 0, 0, 0}
 	};
@@ -79,6 +91,10 @@ int main(int argc, char *argv[]) {
 			case 'a':
 				icon_extension = ".png";
 				break;
+
+			case 'v':
+				display_version();
+				return 0;
 
 			case 'h':
 				display_help();
