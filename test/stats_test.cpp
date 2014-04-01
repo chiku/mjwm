e1b37ab74050df4c0557d9dad21b2bd9246c60e2
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-04-01 22:04:01 +0530

    Migrates stats tests to catch

--- test/stats_test.cpp
+++ test/stats_test.cpp
@@ -16,222 +16,165 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define CATCH_CONFIG_MAIN
+
 #include <iostream>
 #include <string>
 #include <vector>
 
-#include "QUnit.hpp"
+#include "catch.hpp"
 
 #include "stats.h"
 
-namespace amm
+static amm::stats populated_stats()
 {
-  // Verifies the summary of populating a menu
-  class stats_test
-  {
-    QUnit::UnitTest qunit;
+  amm::stats stats;
+  stats.add_classified_file("VLC");
+  stats.add_classified_file("Mousepad");
+  stats.add_classified_file("Firefox");
+  stats.add_unclassified_file("htop");
+  stats.add_unclassified_file("NEdit");
+  stats.add_unparsed_file("daemon");
+  stats.add_suppressed_file("mplayer");
+  return stats;
+}
 
-    void test_stats_doesnt_have_count_by_default()
-    {
-      amm::stats stats;
-      QUNIT_IS_EQUAL(0, stats.total_files());
-      QUNIT_IS_EQUAL(0, stats.total_parsed_files());
-      QUNIT_IS_EQUAL(0, stats.total_unclassified_files());
-      QUNIT_IS_EQUAL(0, stats.total_suppressed_files());
-      QUNIT_IS_EQUAL(0, stats.total_unparsed_files());
+static std::vector<std::string> unhandled_classification_first_set()
+{
+  std::vector<std::string> classifications;
+  classifications.push_back("Calculator");
+  classifications.push_back("Player");
+  classifications.push_back("Archiving");
+  return classifications;
+}
+
+static std::vector<std::string> unhandled_classification_second_set()
+{
+  std::vector<std::string> classifications;
+  classifications.push_back("Browser");
+  classifications.push_back("Player");
+  classifications.push_back("WordProcessor");
+  classifications.push_back("Archiving");
+  return classifications;
+}
+
+static std::string expected_empty_summary()
+{
+  return "Total desktop files: 0\n"
+         "Parsed desktop files: 0\n"
+         "Unparsed desktop files: 0\n"
+         "Suppressed desktop files (NoDisplay): 0\n"
+         "Unclassified desktop files: 0\n";
+}
+
+static std::string expected_short_summary_with_values()
+{
+  return "Total desktop files: 7\n"
+         "Parsed desktop files: 5\n"
+         "Unparsed desktop files: 1\n"
+         "Suppressed desktop files (NoDisplay): 1\n"
+         "Unclassified desktop files: 2\n";
+}
+
+SCENARIO("amm::stats", "[stats]") {
+  GIVEN("A stat") {
+    amm::stats stats;
+
+    WHEN("created") {
+      THEN("it has no total files")        { REQUIRE(stats.total_files() == 0); }
+      THEN("it has no parsed files")       { REQUIRE(stats.total_parsed_files() == 0); }
+      THEN("it has no unclassified files") { REQUIRE(stats.total_unclassified_files() == 0); }
+      THEN("it has no suppressed files")   { REQUIRE(stats.total_suppressed_files() == 0); }
+      THEN("it has no unparsed files")     { REQUIRE(stats.total_unparsed_files() == 0); }
+
+      THEN("is has no counts in short summary") {
+        REQUIRE(stats.short_summary() == expected_empty_summary());
+      }
+
+      THEN("is has no counts in summary") {
+        REQUIRE(stats.summary() == expected_empty_summary());
+      }
+
+      THEN("is has no counts in long summary") {
+        REQUIRE(stats.long_summary() == expected_empty_summary());
+      }
     }
 
-    void test_stats_with_a_classified_file_increments_total_files_and_total_parsed_files()
-    {
-      amm::stats stats;
-      stats.add_classified_file("VLC");
-      QUNIT_IS_EQUAL(1, stats.total_files());
-      QUNIT_IS_EQUAL(1, stats.total_parsed_files());
-      QUNIT_IS_EQUAL(0, stats.total_unclassified_files());
-      QUNIT_IS_EQUAL(0, stats.total_suppressed_files());
-      QUNIT_IS_EQUAL(0, stats.total_unparsed_files());
+    WHEN("a classified file is added") {
+      stats.add_classified_file("file");
+
+      THEN("it has one total file")        { REQUIRE(stats.total_files() == 1); }
+      THEN("it has one parsed file")       { REQUIRE(stats.total_parsed_files() == 1); }
+      THEN("it has no unclassified files") { REQUIRE(stats.total_unclassified_files() == 0); }
+      THEN("it has no suppressed files")   { REQUIRE(stats.total_suppressed_files() == 0); }
+      THEN("it has no unparsed files")     { REQUIRE(stats.total_unparsed_files() == 0); }
     }
 
-    void test_stats_with_an_unclassified_file_increments_total_files_and_total_parsed_files_and_total_unclassified_files()
-    {
-      amm::stats stats;
-      stats.add_unclassified_file("VLC");
-      QUNIT_IS_EQUAL(1, stats.total_files());
-      QUNIT_IS_EQUAL(1, stats.total_parsed_files());
-      QUNIT_IS_EQUAL(1, stats.total_unclassified_files());
-      QUNIT_IS_EQUAL(0, stats.total_suppressed_files());
-      QUNIT_IS_EQUAL(0, stats.total_unparsed_files());
+    WHEN("an unclassified file is added") {
+      stats.add_unclassified_file("file");
+
+      THEN("it has one total file")        { REQUIRE(stats.total_files() == 1); }
+      THEN("it has one parsed file")       { REQUIRE(stats.total_parsed_files() == 1); }
+      THEN("it has one unclassified file") { REQUIRE(stats.total_unclassified_files() == 1); }
+      THEN("it has no suppressed files")   { REQUIRE(stats.total_suppressed_files() == 0); }
+      THEN("it has no unparsed files")     { REQUIRE(stats.total_unparsed_files() == 0); }
     }
 
-    void test_stats_with_an_suppressed_file_increments_total_files_and_total_suppressed_files()
-    {
-      amm::stats stats;
-      stats.add_suppressed_file("VLC");
-      QUNIT_IS_EQUAL(1, stats.total_files());
-      QUNIT_IS_EQUAL(0, stats.total_parsed_files());
-      QUNIT_IS_EQUAL(0, stats.total_unclassified_files());
-      QUNIT_IS_EQUAL(1, stats.total_suppressed_files());
-      QUNIT_IS_EQUAL(0, stats.total_unparsed_files());
+    WHEN("a suppressed file is added") {
+      stats.add_suppressed_file("file");
+
+      THEN("it has one total file")        { REQUIRE(stats.total_files() == 1); }
+      THEN("it has no parsed files")       { REQUIRE(stats.total_parsed_files() == 0); }
+      THEN("it has no unclassified files") { REQUIRE(stats.total_unclassified_files() == 0); }
+      THEN("it has one suppressed file")   { REQUIRE(stats.total_suppressed_files() == 1); }
+      THEN("it has no unparsed files")     { REQUIRE(stats.total_unparsed_files() == 0); }
     }
 
-    void test_stats_with_an_unparsed_file_increments_total_files_and_total_unparsed_files()
-    {
-      amm::stats stats;
-      stats.add_unparsed_file("VLC");
-      QUNIT_IS_EQUAL(1, stats.total_files());
-      QUNIT_IS_EQUAL(0, stats.total_parsed_files());
-      QUNIT_IS_EQUAL(0, stats.total_unclassified_files());
-      QUNIT_IS_EQUAL(0, stats.total_suppressed_files());
-      QUNIT_IS_EQUAL(1, stats.total_unparsed_files());
+    WHEN("an unparsed file is added") {
+      stats.add_unparsed_file("file");
+
+      THEN("it has one total file")        { REQUIRE(stats.total_files() == 1); }
+      THEN("it has no parsed files")       { REQUIRE(stats.total_parsed_files() == 0); }
+      THEN("it has no unclassified files") { REQUIRE(stats.total_unclassified_files() == 0); }
+      THEN("it has no suppressed files")   { REQUIRE(stats.total_suppressed_files() == 0); }
+      THEN("it has one unparsed files")    { REQUIRE(stats.total_unparsed_files() == 1); }
     }
 
-    void test_stats_doesnt_report_any_entries_in_short_summary_by_default()
-    {
-      amm::stats stats;
-      std::string expected = std::string("Total desktop files: 0\n") +
-                             std::string("Parsed desktop files: 0\n") +
-                             std::string("Unparsed desktop files: 0\n") +
-                             std::string("Suppressed desktop files (NoDisplay): 0\n") +
-                             std::string("Unclassified desktop files: 0\n");
-      QUNIT_IS_EQUAL(expected, stats.short_summary());
-    }
-
-    void test_stats_short_summary_includes_counts()
-    {
+    WHEN("different types of files are added") {
       amm::stats stats = populated_stats();
-      std::string expected = std::string("Total desktop files: 7\n") +
-                             std::string("Parsed desktop files: 5\n") +
-                             std::string("Unparsed desktop files: 1\n") +
-                             std::string("Suppressed desktop files (NoDisplay): 1\n") +
-                             std::string("Unclassified desktop files: 2\n");
-      QUNIT_IS_EQUAL(expected, stats.short_summary());
+
+      THEN("short summary includes counts") {
+        REQUIRE(stats.short_summary() == expected_short_summary_with_values());
+      }
+
+      THEN("summary includes short summary and a list of unparsed files") {
+        std::string expected_summary = expected_short_summary_with_values() +
+                                       "List of unparsed files: daemon\n";
+        REQUIRE(stats.summary() == expected_summary);
+      }
+
+      THEN("long summary includes summary and lists of suppressed and unclassified files") {
+        std::string expected_summary = expected_short_summary_with_values() +
+                                       "List of unparsed files: daemon\n"
+                                       "List of suppressed files: mplayer\n"
+                                       "List of unclassified files: htop, NEdit\n";
+        REQUIRE(stats.long_summary() == expected_summary);
+      }
     }
 
-    void test_stats_doesnt_report_any_entries_in_summary_by_default()
-    {
-      amm::stats stats;
-      std::string expected = std::string("Total desktop files: 0\n") +
-                             std::string("Parsed desktop files: 0\n") +
-                             std::string("Unparsed desktop files: 0\n") +
-                             std::string("Suppressed desktop files (NoDisplay): 0\n") +
-                             std::string("Unclassified desktop files: 0\n");
-      QUNIT_IS_EQUAL(expected, stats.summary());
-    }
-
-    void test_stats_summary_includes_short_summary_with_list_of_unparsed_files()
-    {
-      amm::stats stats = populated_stats();
-      std::string expected = std::string("Total desktop files: 7\n") +
-                             std::string("Parsed desktop files: 5\n") +
-                             std::string("Unparsed desktop files: 1\n") +
-                             std::string("Suppressed desktop files (NoDisplay): 1\n") +
-                             std::string("Unclassified desktop files: 2\n") +
-                             std::string("List of unparsed files: daemon\n");
-      QUNIT_IS_EQUAL(expected, stats.summary());
-    }
-
-    void test_stats_doesnt_report_any_entries_in_long_summary_by_default()
-    {
-      amm::stats stats;
-      std::string expected = std::string("Total desktop files: 0\n") +
-                             std::string("Parsed desktop files: 0\n") +
-                             std::string("Unparsed desktop files: 0\n") +
-                             std::string("Suppressed desktop files (NoDisplay): 0\n") +
-                             std::string("Unclassified desktop files: 0\n");
-      QUNIT_IS_EQUAL(expected, stats.long_summary());
-    }
-
-    void test_stats_long_summary_includes_summary_with_list_of_unclassified_files()
-    {
-      amm::stats stats = populated_stats();
-      std::string expected = std::string("Total desktop files: 7\n") +
-                             std::string("Parsed desktop files: 5\n") +
-                             std::string("Unparsed desktop files: 1\n") +
-                             std::string("Suppressed desktop files (NoDisplay): 1\n") +
-                             std::string("Unclassified desktop files: 2\n") +
-                             std::string("List of unparsed files: daemon\n") +
-                             std::string("List of suppressed files: mplayer\n") +
-                             std::string("List of unclassified files: htop, NEdit\n");
-      QUNIT_IS_EQUAL(expected, stats.long_summary());
-    }
-
-    void test_stats_long_summary_includes_list_of_unhandled_classfications_when_present()
-    {
+    WHEN("unhandled classifications are added") {
       amm::stats stats = populated_stats();
       stats.add_unhandled_classifications(unhandled_classification_first_set());
       stats.add_unhandled_classifications(unhandled_classification_second_set());
-      std::string expected = std::string("Total desktop files: 7\n") +
-                             std::string("Parsed desktop files: 5\n") +
-                             std::string("Unparsed desktop files: 1\n") +
-                             std::string("Suppressed desktop files (NoDisplay): 1\n") +
-                             std::string("Unclassified desktop files: 2\n") +
-                             std::string("List of unparsed files: daemon\n") +
-                             std::string("List of suppressed files: mplayer\n") +
-                             std::string("List of unclassified files: htop, NEdit\n") +
-                             std::string("List of unhandled classifications: Archiving, Browser, Calculator, Player, WordProcessor\n");
-      QUNIT_IS_EQUAL(expected, stats.long_summary());
+
+      THEN("long summary includes the unhandled classifications") {
+        std::string expected_summary = expected_short_summary_with_values() +
+                                       "List of unparsed files: daemon\n"
+                                       "List of suppressed files: mplayer\n"
+                                       "List of unclassified files: htop, NEdit\n"
+                                       "List of unhandled classifications: Archiving, Browser, Calculator, Player, WordProcessor\n";
+        REQUIRE(stats.long_summary() == expected_summary);
+      }
     }
-
-    amm::stats populated_stats()
-    {
-      amm::stats stats;
-      stats.add_classified_file("VLC");
-      stats.add_classified_file("Mousepad");
-      stats.add_classified_file("Firefox");
-      stats.add_unclassified_file("htop");
-      stats.add_unclassified_file("NEdit");
-      stats.add_unparsed_file("daemon");
-      stats.add_suppressed_file("mplayer");
-      return stats;
-    }
-
-    std::vector<std::string> unhandled_classification_first_set()
-    {
-      std::vector<std::string> classifications;
-      classifications.push_back("Calculator");
-      classifications.push_back("Player");
-      classifications.push_back("Archiving");
-      return classifications;
-    }
-
-    std::vector<std::string> unhandled_classification_second_set()
-    {
-      std::vector<std::string> classifications;
-      classifications.push_back("Browser");
-      classifications.push_back("Player");
-      classifications.push_back("WordProcessor");
-      classifications.push_back("Archiving");
-      return classifications;
-    }
-
-  public:
-    stats_test(std::ostream &out, int verbose_level) : qunit(out, verbose_level) {}
-
-    int run()
-    {
-      test_stats_doesnt_have_count_by_default();
-      test_stats_with_a_classified_file_increments_total_files_and_total_parsed_files();
-      test_stats_with_an_unclassified_file_increments_total_files_and_total_parsed_files_and_total_unclassified_files();
-      test_stats_with_an_suppressed_file_increments_total_files_and_total_suppressed_files();
-      test_stats_with_an_unparsed_file_increments_total_files_and_total_unparsed_files();
-
-      test_stats_doesnt_report_any_entries_in_short_summary_by_default();
-      test_stats_short_summary_includes_counts();
-
-      test_stats_doesnt_report_any_entries_in_summary_by_default();
-      test_stats_summary_includes_short_summary_with_list_of_unparsed_files();
-
-      test_stats_doesnt_report_any_entries_in_long_summary_by_default();
-      test_stats_long_summary_includes_summary_with_list_of_unclassified_files();
-      test_stats_long_summary_includes_list_of_unhandled_classfications_when_present();
-
-      return qunit.errors();
-    }
-  };
-}
-
-
-int main()
-{
-  return amm::stats_test(std::cerr, QUnit::normal).run();
+  }
 }
