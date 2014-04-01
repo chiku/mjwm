d11bc35aabf536c1d83da44cd2246a83e2b3dbc4
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-04-01 21:08:24 +0530

    Introduces catch for C++ tests
    Migrate tests for amm::subcategory to catch

--- test/subcategory_test.cpp
+++ test/subcategory_test.cpp
@@ -16,82 +16,65 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define CATCH_CONFIG_MAIN
+
 #include <iostream>
 #include <vector>
 
-#include "QUnit.hpp"
+#include "catch.hpp"
 
 #include "desktop_file.h"
 #include "subcategory.h"
 
-namespace amm
+static amm::desktop_file mousepad_desktop_file()
 {
-  // Verifies a collection of desktop files belonging to the same category
-  class subcategory_test
-  {
-    QUnit::UnitTest qunit;
-
-    void test_subcategory_has_no_entries_before_desktop_files_are_added()
-    {
-      subcategory subcategory("Accessories", "accessories", "Utilities");
-      QUNIT_IS_FALSE(subcategory.has_entries());
-    }
-
-    void test_subcategory_has_entries_after_a_desktop_file_is_added()
-    {
-      subcategory subcategory("Accessories", "accessories", "Utilities");
-      subcategory.add_desktop_file(mousepad_desktop_file());
-      QUNIT_IS_TRUE(subcategory.has_entries());
-    }
-
-    void test_subcategory_alphabetically_sorts_desktop_files_by_their_names()
-    {
-      subcategory subcategory("Accessories", "accessories", "Utilities");
-      subcategory.add_desktop_file(sakura_desktop_file());
-      subcategory.add_desktop_file(mousepad_desktop_file());
-      subcategory.sort_desktop_files();
-
-      std::vector<amm::desktop_file> desktop_files = subcategory.desktop_files();
-      QUNIT_IS_EQUAL(2, desktop_files.size());
-      QUNIT_IS_EQUAL("Mousepad", desktop_files[0].name());
-      QUNIT_IS_EQUAL("Sakura", desktop_files[1].name());
-    }
-
-    desktop_file mousepad_desktop_file()
-    {
-      desktop_file entry;
-      entry.populate("Name=Mousepad");
-      entry.populate("Icon=accessories-text-editor");
-      entry.populate("Exec=mousepad %F");
-      entry.populate("Categories=Application;Utility;TextEditor;GTK;");
-      return entry;
-    }
-
-    desktop_file sakura_desktop_file()
-    {
-      desktop_file entry;
-      entry.populate("Name=Sakura");
-      entry.populate("Icon=terminal-tango");
-      entry.populate("Exec=sakura");
-      entry.populate("Categories=GTK;Utility;TerminalEmulator;System;");
-      return entry;
-    }
-
-  public:
-    subcategory_test(std::ostream &out, int verbose_level) : qunit(out, verbose_level) {}
-
-    int run()
-    {
-      test_subcategory_has_no_entries_before_desktop_files_are_added();
-      test_subcategory_has_entries_after_a_desktop_file_is_added();
-      test_subcategory_alphabetically_sorts_desktop_files_by_their_names();
-      return qunit.errors();
-    }
-  };
+  amm::desktop_file entry;
+  entry.populate("Name=Mousepad");
+  entry.populate("Icon=accessories-text-editor");
+  entry.populate("Exec=mousepad %F");
+  entry.populate("Categories=Application;Utility;TextEditor;GTK;");
+  return entry;
 }
 
-
-int main()
+static amm::desktop_file sakura_desktop_file()
 {
-  return amm::subcategory_test(std::cerr, QUnit::normal).run();
+  amm::desktop_file entry;
+  entry.populate("Name=Sakura");
+  entry.populate("Icon=terminal-tango");
+  entry.populate("Exec=sakura");
+  entry.populate("Categories=GTK;Utility;TerminalEmulator;System;");
+  return entry;
+}
+
+SCENARIO("amm::subcategory", "[subcategory]") {
+  GIVEN("A subcategory") {
+    amm::subcategory subcategory("Accessories", "accessories", "Utilities");
+
+    WHEN("without desktop-files") {
+      THEN("it has no entries") {
+        REQUIRE(!subcategory.has_entries());
+      }
+    }
+
+    WHEN("with one desktop-file") {
+      subcategory.add_desktop_file(mousepad_desktop_file());
+      THEN("it has entries") {
+        REQUIRE(subcategory.has_entries());
+      }
+    }
+
+    WHEN("with two desktop-file") {
+      subcategory.add_desktop_file(sakura_desktop_file());
+      subcategory.add_desktop_file(mousepad_desktop_file());
+      WHEN("sorted") {
+        subcategory.sort_desktop_files();
+        THEN("its entries in alphabetical order by name") {
+          std::vector<amm::desktop_file> desktop_files = subcategory.desktop_files();
+          REQUIRE(desktop_files.size() == 2);
+          REQUIRE(desktop_files[0].name() == "Mousepad");
+          REQUIRE(desktop_files[1].name() == "Sakura");
+        }
+      }
+    }
+  }
 }
