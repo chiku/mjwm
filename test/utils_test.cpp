350c6967a718d6fbdacc3ae881c18527d65b34e2
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-04-02 07:16:30 +0530

    Migrates amm::vectorx & amm::stringx tests to catch

--- test/utils_test.cpp
+++ test/utils_test.cpp
@@ -16,154 +16,174 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define CATCH_CONFIG_MAIN
+
 #include <iostream>
 #include <string>
 #include <vector>
 
+#include "catch.hpp"
 #include "QUnit.hpp"
 
 #include "utils.h"
 
-namespace amm
-{
-  class utils_test
-  {
-    QUnit::UnitTest qunit;
+SCENARIO("amm::stringx", "[stringx]") {
+  GIVEN("A stringx with content") {
+    amm::stringx stringx("vlc.desktop");
 
-    void test_stringx_ends_with_a_given_string_if_it_is_present_in_the_end()
-    {
-      QUNIT_IS_TRUE(amm::stringx("vlc.desktop").ends_with(".desktop"));
-      QUNIT_IS_TRUE(amm::stringx(".desktop").ends_with(".desktop"));
+    WHEN("it has a sub-string at the end") {
+      THEN("it ends with the substring") {
+        REQUIRE(stringx.ends_with(".desktop"));
+      }
+
+      THEN("it doesn't end with a substring present inside") {
+        REQUIRE(!stringx.ends_with(".desk"));
+      }
+
+      THEN("it doesn't ends with a superstring") {
+        REQUIRE(!stringx.ends_with("vlc.desktop file"));
+      }
+    }
+  }
+
+  GIVEN("A stringx without a terminating delimeters") {
+    amm::stringx stringx("AudioVideo;GTK");
+
+    WHEN("it is terminated with the delimeter") {
+      std::string result = stringx.terminate_with(";");
+      THEN("it appends the delimeter to the stringx") {
+        REQUIRE(result == "AudioVideo;GTK;");
+      }
     }
 
-    void test_stringx_doesnt_end_with_a_string_if_it_is_not_present_in_the_end()
-    {
-      QUNIT_IS_FALSE(amm::stringx("vlc.desktopfile").ends_with(".desktop"));
-      QUNIT_IS_FALSE(amm::stringx("desktop").ends_with(".desktop"));
+    WHEN("it is split by the delimeter") {
+      std::vector<std::string> result = stringx.split(";");
+      THEN("the the last token is included") {
+        REQUIRE(result.size() == 2);
+        REQUIRE(result[0] == "AudioVideo");
+        REQUIRE(result[1] == "GTK");
+      }
+    }
+  }
+
+  GIVEN("A stringx with a terminating delimeters") {
+    amm::stringx stringx("AudioVideo;GTK;");
+
+    WHEN("it is terminated with the delimeter") {
+      std::string result = stringx.terminate_with(";");
+      THEN("it doesn't duplicate the delimeter") {
+        REQUIRE(result == "AudioVideo;GTK;");
+      }
     }
 
-    void test_stringx_terminate_with_adds_missing_ending_string()
-    {
-      QUNIT_IS_EQUAL("AudioVideo;GTK;", amm::stringx("AudioVideo;GTK").terminate_with(";"));
+    WHEN("it is split by the delimeter") {
+      std::vector<std::string> result = stringx.split(";");
+      THEN("the original string becomes a list of tokens") {
+        REQUIRE(result.size() == 2);
+        REQUIRE(result[0] == "AudioVideo");
+        REQUIRE(result[1] == "GTK");
+      }
     }
+  }
 
-    void test_stringx_terminate_with_doesnt_duplicate_existing_ending_string()
-    {
-      QUNIT_IS_EQUAL("AudioVideo;", amm::stringx("AudioVideo").terminate_with(";"));
+  GIVEN("A empty stringx") {
+    amm::stringx stringx("");
+
+    WHEN("it is terminated with the delimeter") {
+      std::string result = stringx.terminate_with(";");
+      THEN("it adds a delimeter") {
+        REQUIRE(result == ";");
+      }
     }
+  }
 
-    void test_stringx_terminate_with_handles_cases_when_string_length_is_lesser_than_end_length()
-    {
-      QUNIT_IS_EQUAL(";", amm::stringx("").terminate_with(";"));
+  GIVEN("A stringx with XML tags") {
+    amm::stringx stringx("Icon=<\'foo\' & \"bar\">");
+
+    WHEN("it is XML encoded") {
+      std::string result = stringx.encode();
+      THEN("the XML tags are replaced by corresponding XML escpace sequences") {
+        REQUIRE(result == "Icon=&lt;&apos;foo&apos; &amp; &quot;bar&quot;&gt;");
+      }
     }
+  }
 
-    void test_stringx_encode_XML_escapes_characters()
-    {
-      QUNIT_IS_EQUAL("Icon=&lt;&apos;foo&apos; &amp; &quot;bar&quot;&gt;", amm::stringx("Icon=<\'foo\' & \"bar\">").encode());
+  GIVEN("A stringx with whitespaces at extremes") {
+    amm::stringx stringx(" \taccessories-text-editor \t\n");
+
+    WHEN("it is trimmed") {
+      std::string result = stringx.trim();
+      THEN("all whitespaces at the beginning and end are removed") {
+        REQUIRE(result == "accessories-text-editor");
+      }
     }
-
-    void test_stringx_trim_removes_whitespaces_from_begenning_and_end()
-    {
-      QUNIT_IS_EQUAL("accessories-text-editor", amm::stringx(" \taccessories-text-editor \t\n").trim());
-    }
-
-    void test_stringx_split_divides_a_string_with_a_given_delimiter()
-    {
-      std::vector<std::string> result = amm::stringx("AudioVideo;Video;Player;").split(";");
-      QUNIT_IS_EQUAL(3, result.size());
-      QUNIT_IS_EQUAL("AudioVideo", result[0]);
-      QUNIT_IS_EQUAL("Video", result[1]);
-      QUNIT_IS_EQUAL("Player", result[2]);
-    }
-
-    void test_stringx_split_doesnt_leave_out_last_element_if_final_delimeter_is_missing()
-    {
-      std::vector<std::string> result = amm::stringx("AudioVideo;Video;Player").split(";");
-      QUNIT_IS_EQUAL(3, result.size());
-      QUNIT_IS_EQUAL("AudioVideo", result[0]);
-      QUNIT_IS_EQUAL("Video", result[1]);
-      QUNIT_IS_EQUAL("Player", result[2]);
-    }
-
-
-    void test_vectorx_joins_into_a_string_with_a_delimiter()
-    {
-      std::vector<std::string> vector;
-      vector.push_back("foo");
-      vector.push_back("bar");
-      vector.push_back("baz");
-      QUNIT_IS_EQUAL("foo; bar; baz", amm::vectorx(vector).join("; "));
-    }
-
-    void test_vectorx_joins_into_vector_with_one_entry()
-    {
-      std::vector<std::string> vector;
-      vector.push_back("foo");
-      QUNIT_IS_EQUAL("foo", amm::vectorx(vector).join("; "));
-    }
-
-    void test_vectorx_joins_into_vector_with_no_entries()
-    {
-      std::vector<std::string> vector;
-      QUNIT_IS_EQUAL("", amm::vectorx(vector).join("; "));
-    }
-
-    void test_vectorx_terminated_with_an_end_is_applied_to_all_elements()
-    {
-      std::vector<std::string> vector;
-      vector.push_back("foo/");
-      vector.push_back("bar");
-      vector.push_back("baz/");
-
-      std::vector<std::string> terminated_vector = amm::vectorx(vector).terminate_with("/");
-      QUNIT_IS_EQUAL(3, terminated_vector.size());
-      QUNIT_IS_EQUAL("foo/", terminated_vector[0]);
-      QUNIT_IS_EQUAL("bar/", terminated_vector[1]);
-      QUNIT_IS_EQUAL("baz/", terminated_vector[2]);
-    }
-
-    void test_vectorx_unique_removes_duplicates()
-    {
-      std::vector<std::string> vector;
-      vector.push_back("foo");
-      vector.push_back("bar");
-      vector.push_back("foo");
-      vector.push_back("bar");
-      vector.push_back("bar");
-
-      std::vector<std::string> unique_vector = amm::vectorx(vector).unique();
-      QUNIT_IS_EQUAL(2, unique_vector.size());
-      QUNIT_IS_EQUAL("bar", unique_vector[0]);
-      QUNIT_IS_EQUAL("foo", unique_vector[1]);
-    }
-
-  public:
-    utils_test(std::ostream &out, int verbose_level) : qunit(out, verbose_level) {}
-
-    int run()
-    {
-      test_stringx_ends_with_a_given_string_if_it_is_present_in_the_end();
-      test_stringx_doesnt_end_with_a_string_if_it_is_not_present_in_the_end();
-      test_stringx_terminate_with_adds_missing_ending_string();
-      test_stringx_terminate_with_doesnt_duplicate_existing_ending_string();
-      test_stringx_terminate_with_handles_cases_when_string_length_is_lesser_than_end_length();
-      test_stringx_encode_XML_escapes_characters();
-      test_stringx_trim_removes_whitespaces_from_begenning_and_end();
-      test_stringx_split_divides_a_string_with_a_given_delimiter();
-      test_stringx_split_doesnt_leave_out_last_element_if_final_delimeter_is_missing();
-
-      test_vectorx_joins_into_a_string_with_a_delimiter();
-      test_vectorx_joins_into_vector_with_one_entry();
-      test_vectorx_joins_into_vector_with_no_entries();
-      test_vectorx_terminated_with_an_end_is_applied_to_all_elements();
-      test_vectorx_unique_removes_duplicates();
-      return qunit.errors();
-    }
-  };
+  }
 }
 
 
-int main()
-{
-  return amm::utils_test(std::cerr, QUnit::normal).run();
+SCENARIO("amm::vectorx", "[vectorx]") {
+  GIVEN("A vectorx with more than one item") {
+    std::vector<std::string> vector;
+    vector.push_back("foo");
+    vector.push_back("bar");
+    vector.push_back("baz");
+    amm::vectorx vectorx(vector);
+
+    WHEN("joined with a delimeter") {
+      std::string result = vectorx.join("; ");
+      THEN("it returns a string with delimeter in between the items") {
+        REQUIRE(result == "foo; bar; baz");
+      }
+    }
+  }
+
+  GIVEN("A vectorx with one item") {
+    std::vector<std::string> vector;
+    vector.push_back("foo");
+    amm::vectorx vectorx(vector);
+
+    WHEN("joined with a delimeter") {
+      std::string result = vectorx.join("; ");
+      THEN("it returns the item in the original vector") {
+        REQUIRE(result == "foo");
+      }
+    }
+  }
+
+  GIVEN("A vectorx with some items ending in a delimeter") {
+    std::vector<std::string> vector;
+    vector.push_back("foo/");
+    vector.push_back("bar");
+    vector.push_back("baz/");
+    amm::vectorx vectorx(vector);
+
+    WHEN("terminated with the delimeter") {
+      std::vector<std::string> result = vectorx.terminate_with("/");
+      THEN("it adds the delimeter without duplication") {
+        REQUIRE(result.size() == 3);
+        REQUIRE(result[0] == "foo/");
+        REQUIRE(result[1] == "bar/");
+        REQUIRE(result[2] == "baz/");
+      }
+    }
+  }
+
+  GIVEN("A vectorx with duplicates") {
+    std::vector<std::string> vector;
+      vector.push_back("foo");
+      vector.push_back("bar");
+      vector.push_back("foo");
+      vector.push_back("bar");
+      vector.push_back("bar");
+      amm::vectorx vectorx(vector);
+
+    WHEN("unique") {
+      std::vector<std::string> result = amm::vectorx(vector).unique();
+      THEN("it deduplicates") {
+        REQUIRE(result.size() == 2);
+        REQUIRE(result[0] == "bar");
+        REQUIRE(result[1] == "foo");
+      }
+    }
+  }
 }
