87c0d909f585f64d5762f6a52c0eec67551f658a
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-04-11 21:43:58 +0530

    Script to generate a unit test code coverage report

---
+++ coverage.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+validate() {
+  which lcov > /dev/null 2>&1
+  if [ $? != 0 ]
+  then
+    echo "Please install lcov"
+    exit 1
+  fi
+}
+
+reconfigure_with_coverage_support() {
+  CXXFLAGS="-g -O0 --coverage" CFLAGS="-g -O0 --coverage" LDFLAGS="--coverage" ./configure --disable-shared
+}
+
+generate_coverage_output() {
+  make clean
+  make -j8
+  make check -j8
+}
+
+generate_html_report() {
+  lcov --capture --directory src --output-file coverage.info
+  lcov --remove coverage.info "/usr*" --output-file filtered.coverage.info
+  genhtml filtered.coverage.info --num-spaces 4 --output-directory coverage
+}
+
+cleanup() {
+  find . -name *.gcda -delete
+  find . -name *.gcno -delete
+  find . -name *.coverage.info -delete
+}
+
+set -e
+trap "cleanup" SIGINT SIGTERM
+
+validate
+cleanup
+rm -rf coverage
+reconfigure_with_coverage_support
+generate_coverage_output
+generate_html_report
