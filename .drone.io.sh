0f75f31a9796406eebfcc31d8b02e7fdb0761f83
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-05-02 20:15:11 +0530

    Use 3.1.0-pre as version to distinguish it from original the 3.0.0 release

--- .drone.io.sh
+++ .drone.io.sh
@@ -2,14 +2,15 @@
 
 set -e
 
+export CFLAGS="-m32 -Os -Wall -Wextra"
+export CXXFLAGS="-m32 -Os -Wall -Wextra"
+export LDFLAGS="-m32 -Os -Wall -Wextra"
+export VERBOSE=1
+
 reduce_binary_script=$(readlink -f $(dirname $0)/reduce.sh)
 
 sudo apt-get update
 sudo apt-get install g++-multilib upx
-
-export CFLAGS="-Os -Wall -Wextra"
-export CXXFLAGS="-Os -Wall -Wextra"
-export LDFLAGS="-Os -Wall -Wextra"
 
 ./autogen.sh
 ./configure --prefix $HOME/mjwm-install/usr/local
@@ -31,5 +32,6 @@ ls -l
 tar -cvzf mjwm.tar.gz usr
 popd
 cp $HOME/mjwm-install/mjwm.tar.gz mjwm-$mjwm_version.tar.gz
+cp $HOME/mjwm-install/usr/local/bin/mjwm mjwm-$mjwm_version
 
 rm -r $HOME/mjwm-install
