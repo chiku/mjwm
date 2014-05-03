a33b64dd73ecb71687409330d9dfd218556f8649
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-05-03 13:58:10 +0530

    Modularize build script

--- .drone.io.sh
+++ .drone.io.sh
@@ -1,37 +1,53 @@
 #/bin/bash
 
+set_environment() {
+  export VERBOSE=1
+  export CFLAGS="-Os -Wall -Wextra"
+  export CXXFLAGS="-Os -Wall -Wextra"
+  export LDFLAGS="-Os -Wall -Wextra"
+  install_base_dir="$HOME/mjwm-install"
+  install_dir="$install_base_dir/usr/local"
+}
+
+install_prerequisites() {
+  sudo apt-get update
+  sudo apt-get install g++-multilib upx
+}
+
+build() {
+  ./autogen.sh
+  ./configure --prefix $install_dir
+  make clean
+  make -j4
+  make check -j4
+  make install
+}
+
+crunch_binary() {
+  reduce_binary_script=$(readlink -f $(dirname $0)/reduce.sh)
+  pushd $install_dir/bin
+  $reduce_binary_script
+  popd
+}
+
+artifact() {
+  pushd $install_base_dir
+  tar -cvzf mjwm.tar.gz usr
+  popd
+  mjwm_version=$($install_dir/bin/mjwm --version | cut -d' ' -f2)
+  cp $install_base_dir/mjwm.tar.gz mjwm-$mjwm_version.tar.gz
+  cp $install_dir/bin/mjwm mjwm-$mjwm_version
+}
+
+cleanup() {
+  rm -r $install_base_dir
+}
+
 set -e
 
-export CFLAGS="-m32 -Os -Wall -Wextra"
-export CXXFLAGS="-m32 -Os -Wall -Wextra"
-export LDFLAGS="-m32 -Os -Wall -Wextra"
-export VERBOSE=1
-
-reduce_binary_script=$(readlink -f $(dirname $0)/reduce.sh)
-
-sudo apt-get update
-sudo apt-get install g++-multilib upx
-
-./autogen.sh
-./configure --prefix $HOME/mjwm-install/usr/local
-make clean
-make -j4
-make check -j4
-make install
-
-$reduce_binary_script
-
-pushd $HOME/mjwm-install/usr/local/bin
-mjwm_version=$(./mjwm --version | cut -d' ' -f2)
-$reduce_binary_script
-popd
-
-pwd
-pushd $HOME/mjwm-install
-ls -l
-tar -cvzf mjwm.tar.gz usr
-popd
-cp $HOME/mjwm-install/mjwm.tar.gz mjwm-$mjwm_version.tar.gz
-cp $HOME/mjwm-install/usr/local/bin/mjwm mjwm-$mjwm_version
-
-rm -r $HOME/mjwm-install
+set_environment
+install_prerequisites
+build
+crunch_binary
+artifact
+cleanup
