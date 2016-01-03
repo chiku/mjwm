5d086ee0d11d8130d8e3324236edc0a1c5788d44
Chirantan Mitra <chirantan.mitra@gmail.com>
2016-01-03 11:18:54 +0530

    Happy New Year!
    Update copyright years

--- profile.sh
+++ profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This file is part of mjwm.
-# Copyright (C) 2014-2015  Chirantan Mitra <chirantan.mitra@gmail.com>
+# Copyright (C) 2014-2016  Chirantan Mitra <chirantan.mitra@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
