250749ede9f9eb22957adb4406357f368529526e
Chirantan Mitra <chirantan.mitra@gmail.com>
2022-04-21 00:25:36 +0530

    Happy new year!

--- src/menu.cc
+++ src/menu.cc
@@ -1,6 +1,6 @@
 /*
   This file is part of mjwm.
-  Copyright (C) 2014-2016  Chirantan Mitra <chirantan.mitra@gmail.com>
+  Copyright (C) 2014-2022  Chirantan Mitra <chirantan.mitra@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
